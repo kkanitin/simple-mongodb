6ce8cf525279ef1e0e3909ea06ba1d9ca3ef6083
Kanitin <kanitin.kr@gmail.com>
2024-03-16 12:49:47 +0700

    remove indent and line break for condition

--- mongo-init.js
+++ mongo-init.js
@@ -1,29 +1,11 @@
 db = db.getSiblingDB('admin');
 
 // seem like mongodb confuse when use multiple line and indent
-if (db.getUser("springboot-mydatabase") == null) {
-    db.createUser({
-        user: "springboot-mydatabase",
-        pwd: "password",
-        roles: [{"role": "readWrite", "db": "mydatabase"}, {"role": "read", "db": "admin"}, {
-            "role": "read",
-            "db": "config"
-        }]
-    });
-}
+if (db.getUser("springboot-mydatabase") == null) { db.createUser({ user: "springboot-mydatabase", pwd: "password", roles: [ {"role": "readWrite", "db": "mydatabase"}, {"role": "read", "db": "admin"}, {"role": "read", "db": "config"} ] }); }
 
 db = db.getSiblingDB('mydatabase');
 
-if (db.getUser("springboot-mydatabase") == null) {
-    db.createUser({
-        user: "springboot-mydatabase",
-        pwd: "password",
-        roles: [{"role": "readWrite", "db": "mydatabase"}, {"role": "read", "db": "admin"}, {
-            "role": "read",
-            "db": "config"
-        }]
-    });
-}
+if (db.getUser("springboot-mydatabase") == null) { db.createUser({ user: "springboot-mydatabase", pwd: "password", roles: [ {"role": "readWrite", "db": "mydatabase"}, {"role": "read", "db": "admin"}, {"role": "read", "db": "config"} ] }); }
 
 db.createCollection("author")
 
